87cc37699a2d018d3c23d8a57021ad0827dc7833
Aman Bhandal <aman_bhandal@live.ca>
2019-08-06 20:36:01 -0400

    Created view model

---
+++ dotnet-core/AspNetCoreTodo/AspNetCoreTodo/Models/ToDoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+
+namespace AspNetCoreTodo.Views
+{
+    public class ToDoViewModel
+    {
+        public ToDoItem[] Items { get; set; }
+    }
+}
